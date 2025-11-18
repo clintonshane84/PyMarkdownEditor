c3f384320aed7c2753c619ddc4ce8ccdb22224c0
Clinton <clintonshanewright@gmail.com>
2025-11-18 11:58:25 +0200

    Added new web_pdf_exporter.py to include a QWebEngine rendered version of the PDF file, while keeping the existing QTextBrowser version

--- pymd/services/exporters/__init__.py
+++ pymd/services/exporters/__init__.py
@@ -3,5 +3,6 @@
 from .base import ExporterRegistryInst
 from .html_exporter import HtmlExporter
 from .pdf_exporter import PdfExporter
+from .web_pdf_exporter import WebEnginePdfExporter
 
-__all__ = ["ExporterRegistryInst", "HtmlExporter", "PdfExporter"]
+__all__ = ["ExporterRegistryInst", "HtmlExporter", "PdfExporter", "WebEnginePdfExporter"]
